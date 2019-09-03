b888dc9d2813999489d63d17542dbb9d6effa1c4
BlueBug12 <benson.liu.himself@gmail.com>
2019-09-03 21:50:00 +0800

    9. Palindrome Number

---
+++ #9_PailndromeNumber/PailndromeNumber.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s1=to_string(x);
+        string s2=s1;
+        reverse(s1.begin(),s1.end());
+        if(s1==s2)return 1;
+        else return 0;
+    }
+};
